16319ea3f06a4c86fea124d7558232a756817cf8
judos <judos@gmx.ch>
2016-02-12 00:25:56 +0100

    fixed remote call thanks to Rseding91

--- source/control/remote-calls.lua
+++ source/control/remote-calls.lua
@@ -1,22 +1,20 @@
 require "defines"
-
+ 
 function start()
-	local p = game.local_player
-	
-	p.insert{name="basic-mining-drill",count=10}
-	p.insert{name="iron-plate",count=200}
-	p.insert{name="copper-plate",count=200}
-	p.insert{name="basic-transport-belt",count=50}
-	p.insert{name="basic-inserter",count=50}
-	p.insert{name="belt-packager",count=10}
-	p.insert{name="configurable-packager",count=10}
-	p.insert{name="infiniteEnergy",count=1}
-	p.insert{name="medium-electric-pole",count=50}
+	for _,player in pairs(game.players) do
+		player.insert{name="basic-mining-drill",count=10}
+		player.insert{name="iron-plate",count=200}
+		player.insert{name="copper-plate",count=200}
+		player.insert{name="basic-transport-belt",count=50}
+		player.insert{name="basic-inserter",count=50}
+		player.insert{name="belt-packager",count=10}
+		player.insert{name="configurable-packager",count=10}
+		player.insert{name="infiniteEnergy",count=1}
+		player.insert{name="medium-electric-pole",count=50}
+	end
 end
-
-
+ 
+--Use:  /c remote.call("packager","start")
 remote.add_interface("packager", {
-  start
+  start = function() start() end
 })
-
--- /c game.local_player.insert{name="basic-inserter",count=50}
