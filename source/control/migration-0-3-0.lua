3b39d36fc126e898c469f81dbcfbbe1d2bc75117
judos <judos@gmx.ch>
2016-02-16 23:48:11 +0100

    fixed control migration to 0.3.0

--- source/control/migration-0-3-0.lua
+++ source/control/migration-0-3-0.lua
@@ -1,5 +1,6 @@
 function migration_0_3_0()
 	-- remove old data
+	-- global.packagerInputBelts[idEntity] = { idEntity(belt), ... }
 	global.packagerInputBelts=nil
 
 	-- migrate data to new variables
@@ -9,7 +10,7 @@ function migration_0_3_0()
 		if not newSchedule[tick] then newSchedule[tick] = {} end
 		for _,idEntity in pairs(array) do
 			newSchedule[tick][idEntity] = entityOfId(idEntity)
-			global.entityData[idEntity] = entityOfId(idEntity).name
+			global.entityData[idEntity] = {name = entityOfId(idEntity).name}
 		end
 	end
 	global.schedule = newSchedule
